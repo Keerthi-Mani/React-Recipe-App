fd516953fa76f37a299a4d4eedf09f959707e791
Keerthi Mani <carkeane.getz@gmail.com>
2020-01-13 17:38:46 -0500

    api keys are generated

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+  const APP_ID = process.env.REACT_APP_ID;
+  const APP_KEY = process.env.REACT_APP_KEY;
+
+  //GET request
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
