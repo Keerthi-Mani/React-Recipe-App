dc5c09bb7857aaa9330fd421779760b68c192f3a
Keerthi Mani <carkeane.getz@gmail.com>
2020-01-15 08:10:36 -0500

    Updates

---
+++ precache-manifest.13ca02b5e4a9fa00f508b01249bf3bee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfda98e0e54d2fb74e565e69fae3209",
+    "url": "/React-Recipe-App/index.html"
+  },
+  {
+    "revision": "71031ec496fc2682e3f3",
+    "url": "/React-Recipe-App/static/css/main.ba4ba9e0.chunk.css"
+  },
+  {
+    "revision": "a1b5319817f7782a5d2e",
+    "url": "/React-Recipe-App/static/js/2.93308332.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Recipe-App/static/js/2.93308332.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71031ec496fc2682e3f3",
+    "url": "/React-Recipe-App/static/js/main.3c82b39c.chunk.js"
+  },
+  {
+    "revision": "69704e4913db863e1710",
+    "url": "/React-Recipe-App/static/js/runtime-main.00158cd5.js"
+  },
+  {
+    "revision": "b1e89491cb456291f7c01c0f4fff13a3",
+    "url": "/React-Recipe-App/static/media/spice1.b1e89491.jpg"
+  }
+]);
